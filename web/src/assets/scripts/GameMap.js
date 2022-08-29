8552b3c5b56300a99fe8b8c669bfa214951fdd74
=ccccclbs <=603325483@qq.com>
2022-08-29 23:31:13 +0800

    创建菜单与游戏页面

---
+++ web/src/assets/scripts/GameMap.js
@@ -0,0 +1,238 @@
+import { AcGameObject } from "./AcGameObject";
+import { Snake } from "./Snake";
+import { Wall } from "./Wall";
+
+export class GameMap extends AcGameObject{
+    
+    constructor(ctx,parent){
+        super();
+        this.ctx = ctx;
+        this.parent = parent;
+        this.L = 0;
+
+        this.rows = 13;
+        this.cols = 14;
+
+        this.inner_walls_count = 20;
+        this.walls = [];
+
+        this.snakes = [
+            new Snake({id:0,color:"#4876EC",r:this.rows-2,c:1},this),
+            new Snake({id:1,color:"#F94848",r:1,c:this.cols-2},this),
+        ];
+
+        
+
+    }
+
+    check_connectivity(g,sx,sy,tx,ty){
+        if(sx == tx && sy == ty) return true;
+        g[sx][sy] = true;
+
+        let dx = [-1,0,1,0];
+        let dy = [0,1,0,-1];
+        for(let i = 0; i<4; i++){
+            let x = sx+dx[i];
+            let y = sy+dy[i];
+            if(!g[x][y] && this.check_connectivity(g,x,y,tx,ty)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    create_walls(){
+        const g = [];
+        for(let r = 0; r<this.rows; r++ ){
+            g[r]=[];
+            for(let c=0 ; c<this.cols; c++ ){
+                g[r][c] = false;
+
+            }
+        }
+        //给四周加上障碍物
+        for(let r = 0; r<this.rows; r++){
+            g[r][0] = g[r][this.cols-1] = true;
+        }
+
+        for(let c = 0; c<this.cols; c++){
+            g[0][c] = g[this.rows-1][c] = true;
+        }
+
+        //创建随机障碍物
+        for(let i=0; i<this.inner_walls_count/2; i++){
+            for(let j=0; j<1000; j++){
+                let r = parseInt(Math.random() * this.rows);
+                let c = parseInt(Math.random() * this.cols);
+                if(g[r][c] || g[this.rows-1-r][this.cols-1-c]) continue;
+                
+                if( r == this.rows-2 && c==1 || r==1 && c == this.cols-2){
+                    continue;
+                }
+                g[r][c] = g[this.rows-1-r][this.cols-1-c] = true;
+                break;
+
+            }
+        }
+
+        const copy_g = JSON.parse(JSON.stringify(g));
+        if(!this.check_connectivity(copy_g,this.rows-2,1,1,this.cols-2)){
+            return false;
+        }
+
+        for(let r = 0; r<this.rows; r++ ){
+            for(let c=0 ; c<this.cols; c++ ){
+                if(g[r][c]){
+                    this.walls.push(new Wall(r,c,this));
+                }
+
+            }
+        }
+
+    }
+
+    add_listening_events(){
+        this.ctx.canvas.focus();
+
+        // const [snake0,snake1] = this.snakes;
+        const snake0 = this.snakes[0];
+        const snake1 = this.snakes[1];
+        
+        this.ctx.canvas.addEventListener("keydown",e=>{
+            if(e.key==='w'){
+                // console.log(snake0);
+                console.log("w");
+                snake0.set_direction(0);
+                console.log(snake0);
+            }
+            else if (e.key ==='d'){
+                console.log("d");
+                snake0.set_direction(1);
+                console.log(snake0);
+            } 
+            else if (e.key ==='s'){
+                console.log("s");
+               snake0.set_direction(2);
+               console.log(snake0); 
+            } 
+            else if (e.key ==='a'){
+                console.log("a");
+                snake0.set_direction(3);
+                console.log(snake0);
+            } 
+            else if (e.key ==='ArrowUp'){
+                // console.log(snake1);
+                console.log("up");
+                snake1.set_direction(0);
+                console.log(snake1);
+            } 
+            else if (e.key ==='ArrowRight'){
+                console.log("right");
+                snake1.set_direction(1);
+                console.log(snake1);
+            } 
+            else if (e.key ==='ArrowDown'){
+                console.log("down");
+                snake1.set_direction(2);
+                console.log(snake1);
+            } 
+            else if (e.key ==='ArrowLeft'){
+                console.log("left");
+                snake1.set_direction(3);
+                console.log(snake1);
+            } 
+
+        })
+    }
+
+
+
+    start(){
+        for(let i=0; i<100000; i++){
+            if(!this.create_walls()){
+                break;
+            }
+        }
+        this.add_listening_events();
+    }
+
+    update_size(){
+        this.L = parseInt(Math.min(this.parent.clientWidth / this.cols,this.parent.clientHeight / this.rows));
+        this.ctx.canvas.width = this.L * this.cols;
+        this.ctx.canvas.height = this.L * this.rows;
+
+
+    }
+
+    next_step(){ //让两条蛇进入下一回合
+        for(const snake of this.snakes){
+            snake.next_step();
+        }
+    }
+
+    check_ready(){
+        //判断两条蛇是否都准备好下一回合了
+        for(const snake of this.snakes){
+            if(snake.status !== "idle") return false;
+            if(snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    check_valid(cell){ //检测目标位置是否合法：没有撞到两条蛇的身体和障碍物
+        for(const wall of this.walls){
+            if(wall.r === cell.r && wall.c === cell.c){
+                return false;
+            }
+        }
+
+        for(const snake of this.snakes){
+            let k = snake.cells.length;
+            if(!snake.check_taill_increasing()){ //当蛇尾会前进的时候，蛇尾不要判断
+                k--;
+            }
+            for(let i = 0; i < k; i++){
+                if(snake.cells[i].r === cell.r && snake.cells[i].c === cell.c){
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+
+    }
+
+    update(){
+        this.update_size();
+        if(this.check_ready()){
+            console.log("debug update");
+            this.next_step();
+        }
+        this.render();
+    }
+
+    render(){
+        // this.ctx.fillStyle = 'green';
+        // this.ctx.fillRect(0,0,this.ctx.canvas.width,this.ctx.canvas.height);
+        // // console.log("abcd");
+
+        const color_even = "#AAD751", color_odd = "#A2D149";
+
+        for(let r = 0; r < this.rows; r++){
+            for(let c = 0; c<this.cols; c++){
+                if((r + c) % 2 == 0){
+                    // console.log("1");
+                    this.ctx.fillStyle = color_even;
+                }else{
+                    // console.log("2");
+                    this.ctx.fillStyle = color_odd;
+                }
+                this.ctx.fillRect(c * this.L, r * this.L,this.L,this.L);
+            }
+        }
+
+
+    }
+
+}
