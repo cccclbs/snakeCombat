6750f2a791cc908b476f6da2b2b114e1a55ffae0
=ccccclbs <=603325483@qq.com>
2022-09-05 00:51:46 +0800

    匹配系统上

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -4,10 +4,11 @@ import { Wall } from "./Wall";
 
 export class GameMap extends AcGameObject{
     
-    constructor(ctx,parent){
+    constructor(ctx,parent,store){
         super();
         this.ctx = ctx;
         this.parent = parent;
+        this.store = store;
         this.L = 0;
 
         this.rows = 13;
@@ -25,60 +26,62 @@ export class GameMap extends AcGameObject{
 
     }
 
-    check_connectivity(g,sx,sy,tx,ty){
-        if(sx == tx && sy == ty) return true;
-        g[sx][sy] = true;
+    // check_connectivity(g,sx,sy,tx,ty){
+    //     if(sx == tx && sy == ty) return true;
+    //     g[sx][sy] = true;
 
-        let dx = [-1,0,1,0];
-        let dy = [0,1,0,-1];
-        for(let i = 0; i<4; i++){
-            let x = sx+dx[i];
-            let y = sy+dy[i];
-            if(!g[x][y] && this.check_connectivity(g,x,y,tx,ty)){
-                return true;
-            }
-        }
-        return false;
-    }
+    //     let dx = [-1,0,1,0];
+    //     let dy = [0,1,0,-1];
+    //     for(let i = 0; i<4; i++){
+    //         let x = sx+dx[i];
+    //         let y = sy+dy[i];
+    //         if(!g[x][y] && this.check_connectivity(g,x,y,tx,ty)){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 
     create_walls(){
-        const g = [];
-        for(let r = 0; r<this.rows; r++ ){
-            g[r]=[];
-            for(let c=0 ; c<this.cols; c++ ){
-                g[r][c] = false;
+        const g = this.store.state.pk.gamemap;
 
-            }
-        }
-        //给四周加上障碍物
-        for(let r = 0; r<this.rows; r++){
-            g[r][0] = g[r][this.cols-1] = true;
-        }
+        // const g = [];
+        // for(let r = 0; r<this.rows; r++ ){
+        //     g[r]=[];
+        //     for(let c=0 ; c<this.cols; c++ ){
+        //         g[r][c] = false;
 
-        for(let c = 0; c<this.cols; c++){
-            g[0][c] = g[this.rows-1][c] = true;
-        }
+        //     }
+        // }
+        // //给四周加上障碍物
+        // for(let r = 0; r<this.rows; r++){
+        //     g[r][0] = g[r][this.cols-1] = true;
+        // }
 
-        //创建随机障碍物
-        for(let i=0; i<this.inner_walls_count/2; i++){
-            for(let j=0; j<1000; j++){
-                let r = parseInt(Math.random() * this.rows);
-                let c = parseInt(Math.random() * this.cols);
-                if(g[r][c] || g[this.rows-1-r][this.cols-1-c]) continue;
+        // for(let c = 0; c<this.cols; c++){
+        //     g[0][c] = g[this.rows-1][c] = true;
+        // }
+
+        // //创建随机障碍物
+        // for(let i=0; i<this.inner_walls_count/2; i++){
+        //     for(let j=0; j<1000; j++){
+        //         let r = parseInt(Math.random() * this.rows);
+        //         let c = parseInt(Math.random() * this.cols);
+        //         if(g[r][c] || g[this.rows-1-r][this.cols-1-c]) continue;
                 
-                if( r == this.rows-2 && c==1 || r==1 && c == this.cols-2){
-                    continue;
-                }
-                g[r][c] = g[this.rows-1-r][this.cols-1-c] = true;
-                break;
+        //         if( r == this.rows-2 && c==1 || r==1 && c == this.cols-2){
+        //             continue;
+        //         }
+        //         g[r][c] = g[this.rows-1-r][this.cols-1-c] = true;
+        //         break;
 
-            }
-        }
+        //     }
+        // }
 
-        const copy_g = JSON.parse(JSON.stringify(g));
-        if(!this.check_connectivity(copy_g,this.rows-2,1,1,this.cols-2)){
-            return false;
-        }
+        // const copy_g = JSON.parse(JSON.stringify(g));
+        // if(!this.check_connectivity(copy_g,this.rows-2,1,1,this.cols-2)){
+        //     return false;
+        // }
 
         for(let r = 0; r<this.rows; r++ ){
             for(let c=0 ; c<this.cols; c++ ){
@@ -88,6 +91,8 @@ export class GameMap extends AcGameObject{
 
             }
         }
+
+
 
     }
 
@@ -148,11 +153,14 @@ export class GameMap extends AcGameObject{
 
 
     start(){
-        for(let i=0; i<100000; i++){
-            if(!this.create_walls()){
-                break;
-            }
-        }
+        // for(let i=0; i<100000; i++){
+        //     if(!this.create_walls()){
+        //         break;
+        //     }
+        // }
+
+        this.create_walls();
+
         this.add_listening_events();
     }
 
